601f80752be5bc4fc6f59155adc60d17c16e55b0
xuanyue_18 <1820921675@qq.com>
2024-03-06 10:20:48 +0800

    feat: 添加建表语句

---
+++ sql/create_table.sql
@@ -0,0 +1,78 @@
+# 数据库初始化
+# @author xuanyue18
+
+-- 创建库
+create database if not exists xoj_db
+
+-- 切换库
+use xoj_db;
+
+-- 用户表
+create table user
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    userAccount  varchar(256)                           not null comment '账号',
+    userPassword varchar(512)                           not null comment '密码',
+    unionId      varchar(256)                           null comment '微信开放平台id',
+    mpOpenId     varchar(256)                           null comment '公众号openId',
+    userName     varchar(256)                           null comment '用户昵称',
+    userAvatar   varchar(1024)                          null comment '用户头像',
+    userProfile  varchar(512)                           null comment '用户简介',
+    userRole     varchar(256) default 'user'            not null comment '用户角色：user/admin/ban',
+    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete     tinyint      default 0                 not null comment '是否删除'
+)
+    comment '用户' collate = utf8mb4_unicode_ci;
+
+create index idx_unionId
+    on user (unionId);
+-- 题目表
+create table question
+(
+    id          bigint auto_increment comment 'id'
+        primary key,
+    title       varchar(512)                       null comment '标题',
+    content     text                               null comment '内容',
+    tags        varchar(1024)                      null comment '标签列表（json 数组）',
+    answer      text                               null comment '题目答案',
+    submitNum   int      default 0                 not null comment '题目提交数',
+    acceptedNum int      default 0                 not null comment '题目通过数',
+    judgeCase   text                               null comment '判题用例（json 数组）',
+    judgeConfig text                               null comment '判题配置（json 对象）',
+    difficulty  tinyint  default 0                 null comment '题目难度（0 - 简单、1 - 中等、2 - 困难）',
+    thumbNum    int      default 0                 not null comment '点赞数',
+    favourNum   int      default 0                 not null comment '收藏数',
+    userId      bigint                             not null comment '创建用户 id',
+    createTime  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete    tinyint  default 0                 not null comment '是否删除'
+)
+    comment '题目' collate = utf8mb4_unicode_ci;
+create index idx_userId
+    on question (userId);
+
+-- 题目提交表
+create table question_submit
+(
+    id         bigint auto_increment comment 'id'
+        primary key,
+    language   varchar(128)                       not null comment '编程语言',
+    code       text                               not null comment '用户代码',
+    judgeInfo  text                               null comment '判题信息（json 对象）',
+    status     int      default 0                 not null comment '判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）',
+    questionId bigint                             not null comment '题目 id',
+    userId     bigint                             not null comment '创建用户 id',
+    createTime datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    updateTime datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    isDelete   tinyint  default 0                 not null comment '是否删除'
+)
+    comment '题目提交';
+
+create index idx_questionId
+    on question_submit (questionId);
+
+create index idx_userId
+    on question_submit (userId);
+
